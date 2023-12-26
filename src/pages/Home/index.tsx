60dacf7cbd990334f0a553907e351de084450ab7
Tércio Souza de Jesus <terciodejesus@gmail.com>
2023-12-26 08:55:22 -0300

    style: start input style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCoutdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,10 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput type="number" id="minutesAmount" />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +31,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCoutdownButton type="submit">
           <Play />
           Começar
-        </button>
+        </StartCoutdownButton>
       </form>
     </HomeContainer>
   )
